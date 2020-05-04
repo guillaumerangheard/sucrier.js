49199aaeee3c795b4356b4588853cebd2b599c8d
Guillaume Rangheard <guillaume.rangheard@gmail.com>
2020-05-04 07:57:26 +0200

    Update sucrier.js

--- sucrier.js
+++ sucrier.js
@@ -1,3 +1,37 @@
+(function(M,N,Np){
+	var _e=function(a,b){for(var k in b){a[k]=a[k]||b[k];}};
+	_e(M,{
+		acosh:function(x){return M.log(x+M.sqrt(x*x-1));},
+		asinh:function(x){return -Infinity===x?x:M.log(x+M.sqrt(x*x+1));},
+		atanh:function(x){return M.log((1+x)/(1-x))/2;},
+		cbrt:function(x){var y=M.pow(M.abs(x),1/3);return x<0?-y:y;},
+		cosh:function(x){return (M.exp(x)+M.exp(-x))/2;},
+		expm1:function(x){return M.exp(x)-1;},
+		log10:function(x){return M.log(x)*M.LOG10E;},
+		sinh:function(x){var y=M.exp(x);return (y-1/y)/2;},
+		tanh:function(x){if(Infinity===x){return 1;}if(-Infinity===x){return -1;}var y=M.exp(2*x);return (y-1)/(y+1);}
+	});
+	
+	_e(Np,{
+		atan:function(x){return arguments.length?M.atan2(this,x):M.atan(this);},
+		pow:function(x){return M.pow(this,x);}
+	});
+	
+	var _f=[
+			"abs","acos","acosh","asin","asinh","atanh",
+			"cbrt","cos","cosh",
+			"exp","expm1",
+			"log",
+			"sin","sinh",
+			"tan","tanh"
+		],
+		i=-1,
+		l=_f.length;
+	while(++i<l){
+		Np[_f[i]]=new Function("return Math."+_f[i]+"(this);");
+	}
+})(Math,Number,Number.prototype);
+
 (function(S,Sp){
 	
 	Sp.at=function(a){
